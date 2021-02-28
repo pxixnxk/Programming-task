870568c80d1345bf9727a03b703620f375d353dc
pxixnxk <58324599+pxixnxk@users.noreply.github.com>
2021-02-28 19:03:19 +0800

    Update Q3_Solution.py

--- Q3_Solution.py
+++ Q3_Solution.py
@@ -2,6 +2,7 @@ import csv
 from requests_html import HTMLSession
 import requests
 import json
+import time
 
 # store properties and the corresponding ID
 propertyIdMap = [
@@ -23,25 +24,29 @@ propertyIdMap = [
 ]
 
 outputFile = 'properties.csv'
+session = HTMLSession()
 
 # Using HTMLSession to get the details
 def crawl(issuekey):
-    url = "https://issues.apache.org/jira/browse/CAMEL-" + issuekey
-    session = HTMLSession()
+    url = "https://issues.apache.org/jira/browse/CAMEL-{}".format(issuekey)
+    print(url)
     r = session.get(url)
 
     # Convert from JSON to Python dictionary and get comments
-    url_com = "https://issues.apache.org/jira/rest/api/2/issue/CAMEL-"+issuekey+"/comment"
+    url_com = "https://issues.apache.org/jira/rest/api/2/issue/CAMEL-{}/comment".format(issuekey)
     resp = requests.get(url_com)
     com_json = json.loads(resp.text)
     comments = ""
-    for i in range(0, com_json["total"]):  # num of comments
-        comments += com_json["comments"][i]["updateAuthor"]["displayName"] + ":" + com_json["comments"][i]["created"] + ":" + com_json["comments"][i]["body"]
+    try:
+        for i in range(0, com_json["total"]):  # num of comments
+            comments += com_json["comments"][i]["updateAuthor"]["displayName"] + ":" + com_json["comments"][i]["created"] + ":" + com_json["comments"][i]["body"]
+    except KeyError:
+        print(com_json)
 
     # Select only elements containing certain properties and write to .csv file
-    with open(outputFile, 'a') as csvFile:
+    with open(outputFile, 'a+') as csvFile:
         writer = csv.writer(csvFile)
-        vals = []
+        vals = ["CAMEL-{}".format(issuekey)]
         for name, elementId in propertyIdMap:
             value = r.html.find(elementId)
             if (not value):
@@ -57,7 +62,7 @@ def crawl(issuekey):
 # create the .csv file and input the headings
 def createCsv():
     with open(outputFile, 'w', newline='') as csvfile:
-        fieldnames = []
+        fieldnames = ["Issue"]
         for name in propertyIdMap:
             fieldnames.append(name[0])
         fieldnames.append("Comments")
@@ -66,6 +71,13 @@ def createCsv():
 
 # input the key value(a number) of the issue to get the report
 if __name__ == "__main__":
-    issuekey = input("Please input the issue key:")
+    # issuekey = input("Please input the issue key:")
     createCsv()
-    crawl(issuekey)
+    for issuekey in range(1, 16270):
+        try:
+            crawl(issuekey)
+            time.sleep(0.2)
+        except requests.exceptions.ProxyError:
+            time.sleep(0.5)
+
+
